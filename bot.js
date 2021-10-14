8f22f54910a994666de0779db53d08216519e857
Vamshi Krishna Hasanabada <53006565+vamshikrishnafive@users.noreply.github.com>
2021-10-14 20:36:06 +0530

    intii

---
+++ bot.js
@@ -0,0 +1,12 @@
+const {Telegraf} = require('telegraf');
+
+const bot = new Telegraf('2091233490:AAFxEKf7MKPWHLjqi5c4p4SvAttYjlM_Eok');
+
+// bot.hears('hi', (ctx) => {
+//     console.log(ctx.chat)
+//     ctx.reply('Hey there')
+// })
+
+bot.on()
+
+bot.launch();
